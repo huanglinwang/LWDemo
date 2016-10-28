07e2075500db6a815f1add297a730805fb2804ce
LinWang <15714674@qq.com>
2016-10-28 18:58:24 +0800

    初始化

---
+++ LWDemo/LWDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LWDemo
+//
+//  Created by huanglinwang on 16/10/28.
+//  Copyright © 2016年 Linwang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
